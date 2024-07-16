694a08372b04e7d6f05b1488ea4e85a4eb2e3d13
ammar4753 <55083620+ammar4753@users.noreply.github.com>
2024-07-16 13:54:53 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,26 @@
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain import PromptTemplate
 
-st.set_page_config(page_title="🦜🔗 Blog Outline Generator App")
-st.title('🦜🔗 Blog Outline Generator App')
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+st.title("Echo Bot")
 
-def generate_response(topic):
-  llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
-  # Prompt
-  template = 'As an experienced data scientist and technical writer, generate an outline for a blog about {topic}.'
-  prompt = PromptTemplate(input_variables=['topic'], template=template)
-  prompt_query = prompt.format(topic=topic)
-  # Run LLM model and print out response
-  response = llm(prompt_query)
-  return st.info(response)
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-with st.form('myform'):
-  topic_text = st.text_input('Enter keyword:', '')
-  submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(topic_text)
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
